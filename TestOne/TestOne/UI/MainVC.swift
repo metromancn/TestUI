eb810d090b31191c3273e33ae8c519ad59b11065
metromancn <metromancn@gmail.com>
2019-04-14 13:19:28 +0900

    add view

--- TestOne/TestOne/UI/MainVC.swift
+++ TestOne/TestOne/UI/MainVC.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 class MainVC: PopMetroVC {
+    var view1: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = true
         view.backgroundColor = UIColor.groupTableViewBackground
         initMetroVC(MMFrame.getFrame(status: false, nav: false, tab: true), MMFile.joinPath(MMFile.appPath(), "routemap_sh_cn.pdf"))
-        //MMPdf.zoom(self.pdfView, 1.0)
+        //
+        view1 = UIView(frame: CGRect(x: 0, y: view.frame.height / 2, width: view.frame.width, height: view.frame.height))
+        view1.backgroundColor = UIColor.blue
+        view.addSubview(view1)
     }
 }
