16026eef4742d60df709d7e110bcc7bd20d1a2e1
metromancn <metromancn@gmail.com>
2019-04-16 19:42:57 +0900

    fix simple ok

--- TestTwo/TestTwo/ViewController.swift
+++ TestTwo/TestTwo/ViewController.swift
@@ -1,20 +1,26 @@
-//
-//  ViewController.swift
-//  TestTwo
-//
-//  Created by WUQIUPING on 2019/04/16.
-//  Copyright © 2019 MetroMan. All rights reserved.
-//
-
 import UIKit
+import FloatingPanel
 
 class ViewController: UIViewController {
-
+    var floatingPanelController: FloatingPanelController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        floatingPanelController = FloatingPanelController()
+        let semiModalViewController = SemiModalViewController()
+        floatingPanelController.set(contentViewController: semiModalViewController)
+        floatingPanelController.addPanel(toParent: self, belowView: nil, animated: false)
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        floatingPanelController.removePanelFromParent(animated: true)
+    }
 }
 
+class SemiModalViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orange
+    }
+}
